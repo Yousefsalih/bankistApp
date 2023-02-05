7426d6c526aff3e03a30196ba640f5dfd728d010
yousef <yousef_salih@hotmail.com>
2023-02-05 16:51:26 +0300

    Display total deposit, withdrawal, and interest

--- script.js
+++ script.js
@@ -66,8 +66,10 @@ const displayMovements = function(movements) {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
     const html = `
           <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-          <div class="movements__value">${mov}</div>
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}€</div>
         </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html) //4 different types of inputs (afterbegin, before end, etc.) The order of element matters to choose the right one//html is the variable above
@@ -79,11 +81,34 @@ displayMovements(account1.movements); //call the function
 //Display final balance
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
   console.log(balance); //3840 for the first account
 };
 
 calcDisplayBalance(account1.movements)
+
+//Display the summary of the totals
+const calcDepositSummary = function(movements) {
+  //Display the income
+  const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`
+
+//Display the withdrawal amount
+const withdrawal = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(withdrawal)}€`;
+
+//Display the interest total
+const interest = movements
+.filter(mov => mov > 0)
+.map(deposit => (deposit * 1.2) / 100).filter((int, i, arr) => {
+  console.log(arr);
+  return int >= 1;
+})
+.reduce((acc, int) => acc + int, 0);
+labelSumInterest.textContent = `${interest}€`;
+}
+
+calcDepositSummary(account1.movements);
 
 //Adding a username property for all the accounts based on the initials of the owner name:
 const createUsernames = function (accs) { //Array of accounts
