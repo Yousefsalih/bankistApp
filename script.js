0ecc9fc527e1449bd69f56f546ecb2cd58a5e3ef
yousef <yousef_salih@hotmail.com>
2023-02-12 17:44:09 +0300

    Formatting date

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ const account1 = {
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-02-08T23:36:17.929Z',
+    '2023-02-10T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -100,6 +100,22 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const formatMovementDate = function(date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+    const daysPassed = calcDaysPassed(new Date(), date);
+    console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+  //Adding the dates for the movements
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}}
 //Show the transactions
 const displayMovements = function(acc, sort = false) {
 
@@ -110,14 +126,8 @@ const displayMovements = function(acc, sort = false) {
 
   movs.forEach(function (mov, i){
     const type = mov > 0 ? 'deposit' : 'withdrawal';
-    //Adding the dates for the movements
     const date = new Date(acc.movementsDates[i]);
-    const now = new Date();
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`
-
+    const displayDate = formatMovementDate(date);
     const html = `
           <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
