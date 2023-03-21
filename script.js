e755fdf18a65614987999dc932ae26aad875442b
yousef <yousef_salih@hotmail.com>
2023-03-21 11:18:38 +0300

    Adding user login info and changing timer

--- script.js
+++ script.js
@@ -41,38 +41,7 @@ const account2 = {
   locale: 'en-US',
 };
 
-const accounts = [account1, account2];
-
-
-// const account1 = {
-//   owner: 'Jonas Schmedtmann',
-//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-//   interestRate: 1.2, // %
-//   pin: 1111,
-// };
-
-// const account2 = {
-//   owner: 'Jessica Davis',
-//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-//   interestRate: 1.5,
-//   pin: 2222,
-// };
-
-// const account3 = {
-//   owner: 'Steven Thomas Williams',
-//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
-//   interestRate: 0.7,
-//   pin: 3333,
-// };
-
-// const account4 = {
-//   owner: 'Sarah Smith',
-//   movements: [430, 1000, 700, 50, 90],
-//   interestRate: 1,
-//   pin: 4444,
-// };
-
-// const accounts = [account1, account2, account3, account4]; //Holding accounts array
+const accounts = [account1, account2]; //holding accounts array
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -268,7 +237,7 @@ const startLogOutTimer = function() {
   };
 
   //Set time to 5 minutes
-  let time = 10;
+  let time = 300;
   tick();
   //Call the timer every second
   const timer = setInterval(tick, 1000);
